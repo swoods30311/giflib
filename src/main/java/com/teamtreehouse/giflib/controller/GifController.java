e1fcdaf79b66eba47ba7cc3e7390168aebae46b7
swoods30311 <swoods30311@gmail.com>
2021-06-26 23:27:26 -0400

    first commit

---
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -0,0 +1,35 @@
+package com.teamtreehouse.giflib.controller;
+
+import com.teamtreehouse.giflib.data.GifRepository;
+import com.teamtreehouse.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+public class GifController {
+    @Autowired
+    private GifRepository gifRepository;
+
+    //List the most recent animated gifs in the homepg
+    // @ RequestMapping maps a certain URI request to a method
+    @RequestMapping("/")
+    public String listGifs(ModelMap modelMap) {
+        List<Gif> allGifs = gifRepository.getAllGifs();
+        modelMap.put("gifs",allGifs);
+        return "home";
+    }
+
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap) {
+        Gif gif = gifRepository.findByName(name);
+        modelMap.put("gif",gif);
+        return "gif-details";
+    }
+}
